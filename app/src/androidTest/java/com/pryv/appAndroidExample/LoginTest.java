e9d1365649e6bf18e0e011ab43afe7392a3ff6f4
Thiebaud Modoux <thiebaud.modoux@epfl.ch>
2016-04-05 15:01:32 +0200

    Lib update, web espresso for webview testing

--- app/src/androidTest/java/com/pryv/appAndroidExample/LoginTest.java
+++ app/src/androidTest/java/com/pryv/appAndroidExample/LoginTest.java
@@ -4,8 +4,6 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
-import com.pryv.appAndroidExample.activities.LoginActivity;
-
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,7 +13,6 @@ import org.junit.runner.RunWith;
  * Created by Thieb on 30.03.2016.
  */
 @RunWith(AndroidJUnit4.class)
-@LargeTest
 public class LoginTest {
     private String userName;
     private String validPassword;
