f30fe1c3248e7d50c4c5f1d113ed02a55ca00654
Thiebaud Modoux <thiebaud.modoux@epfl.ch>
2016-04-06 11:02:36 +0200

    Espresso tests

--- app/src/androidTest/java/com/pryv/appAndroidExample/LoginTest.java
+++ app/src/androidTest/java/com/pryv/appAndroidExample/LoginTest.java
@@ -1,5 +1,6 @@
 package com.pryv.appAndroidExample;
 
+import android.content.Intent;
 import android.support.test.espresso.web.webdriver.DriverAtoms;
 import android.support.test.espresso.web.webdriver.Locator;
 import android.support.test.rule.ActivityTestRule;
@@ -10,10 +11,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.web.assertion.WebViewAssertions.webMatches;
 import static android.support.test.espresso.web.sugar.Web.onWebView;
 import static android.support.test.espresso.web.webdriver.DriverAtoms.clearElement;
 import static android.support.test.espresso.web.webdriver.DriverAtoms.findElement;
+import static android.support.test.espresso.web.webdriver.DriverAtoms.getText;
 import static android.support.test.espresso.web.webdriver.DriverAtoms.webClick;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -28,7 +32,7 @@ public class LoginTest {
     private String invalidPassword;
 
     @Rule
-    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<LoginActivity>(LoginActivity.class, false, true) {
+    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<LoginActivity>(LoginActivity.class, false, false) {
         @Override
         protected void afterActivityLaunched() {
             // Enable JS!
@@ -39,12 +43,14 @@ public class LoginTest {
     @Before
     public void initCredentials() {
         userName = "tmodoux";
-        validPassword = "pryv2016";
         invalidPassword = "password";
     }
 
     @Test
     public void invalidLogin() {
+        // Lazily launch the Activity with a custom start Intent per test
+        mActivityRule.launchActivity(new Intent());
+
         onWebView()
                 // Find the input element by ID for username
                 .withElement(findElement(Locator.ID, "loginUsernameOrEmail"))
@@ -63,29 +69,8 @@ public class LoginTest {
                         // Simulate a click via javascript
                 .perform(webClick());
 
-
-    }
-
-    @Test
-    public void validLogin() {
-        onWebView()
-                // Find the input element by ID for username
-                .withElement(findElement(Locator.ID, "loginUsernameOrEmail"))
-                        // Clear previous input
-                .perform(clearElement())
-                        // Enter text into the input element
-                .perform(DriverAtoms.webKeys(userName))
-                        // Same for password
-                .withElement(findElement(Locator.ID, "loginPassword"))
-                        // Clear previous input
-                .perform(clearElement())
-                        // Enter text into the input element
-                .perform(DriverAtoms.webKeys(validPassword))
-                        // Find the submit button
-                .withElement(findElement(Locator.ID, "login-form-loginButton"))
-                        // Simulate a click via javascript
-                .perform(webClick());
-
+        assertTrue(mActivityRule.getActivity().getUsername() == null);
+        assertTrue(mActivityRule.getActivity().getToken() == null);
 
     }
 
